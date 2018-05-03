a9eca0f6140a167b01f1bfe9d7a2835fefda606d
Syed Aman <sna274@nyu.edu>
2018-05-03 15:22:19 -0500

    update API

--- main.go
+++ main.go
@@ -1,31 +1,18 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"fmt"
 	"context"
-	"time"
-	"os"
+	"flag"
+	"fmt"
 	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
+	"time"
 )
-
-func main() {
-	nextRequestID := func() string {
-		return fmt.Sprintf("%d", time.Now().UnixNano())
-	}
-
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-
-	server := &http.Server{
-		Addr: ":9000",
-		Handler: tracing(nextRequestID)(logging(logger)(setupRoutes())),
-		ReadTimeout: 10000,
-		WriteTimeout: 10000,
-		MaxHeaderBytes: 1 << 20,
-	}
-	log.Fatal(server.ListenAndServe())
-}
 
 type key int
 
@@ -33,43 +20,65 @@ const (
 	requestIDKey key = 0
 )
 
-func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestID := r.Header.Get("X-Request-Id")
-			if requestID == "" {
-				requestID = nextRequestID()
-			}
-			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
-			w.Header().Set("X-Request-Id", requestID)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
+var (
+	listenAddr string
+	healthy int32
+)
+
+func main() {
+	flag.StringVar(&listenAddr, "listen-addr", ":9000", "server listen address")
+	flag.Parse()
+	
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	
+	nextRequestID := func() string {
+		return fmt.Sprintf("%d", time.Now().UnixNano())
 	}
-}
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
-}
-
-func logging(logger *log.Logger) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				requestID, ok := r.Context().Value(requestIDKey).(string)
-				if !ok {
-					requestID = "unknown"
-				}
-				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
-			}()
-			next.ServeHTTP(w, r)
-		})
+	server := &http.Server{
+		Addr: listenAddr,
+		Handler: tracing(nextRequestID)(logging(logger)(routes())),
+		ErrorLog: logger,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout: 15 * time.Second,
 	}
+
+	done := make(chan bool)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		logger.Println("Server is shutting down...")
+		atomic.StoreInt32(&healthy, 0)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+		defer cancel()
+
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		close(done)
+	}()
+
+	logger.Println("Server is ready to handle requests at", listenAddr)
+	atomic.StoreInt32(&healthy, 1)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
+	}
+	
+	<-done
+	logger.Println("Server stopped")
 }
 
-func setupRoutes() *http.ServeMux {
+func routes() *http.ServeMux {
 	router := http.NewServeMux()
 	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/health", healthHandler)
 	router.HandleFunc("/hello", helloHandler)
+	router.HandleFunc("/json-as-text", forceTextHandler)
 	return router
 }
 
@@ -79,8 +88,17 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	const inlineTemplate = `
-		<h1>Ecommerce</h1>
+	var inlineTemplate = `
+		<h1>{{ .Name : SayHi }}</h1>
+		<p>{{ CurrentTime }}</p>
+		<p>Your IP: {{ .IP }}</p>
+		<ul>
+			{{ range $key, $value := .Links }}
+			<li>
+				<a href="{{ $value }}">{{ $key }}</a>
+			</li>
+			{{ end }}
+		</ul>
 	`
 
 	data := struct {
@@ -93,6 +111,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		Links: map[string]string{
 			"Home": "/",
 			"Hello": "/hello",
+			"Health Ping": "/health",
+			"JSON as TEXT": "/json-as-text",
 		},
 	}
 
@@ -108,5 +128,53 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err := tmpl.Execute(w, data); err != nil {
 		fmt.Println(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func forceTextHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "{\"status\":\"ok\"}")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	if atomic.LoadInt32(&healthy) == 1 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.WriteHeader(http.StatusServiceUnavailable)
+}
+
+func logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				requestID, ok := r.Context().Value(requestIDKey).(string)
+				if !ok {
+					requestID = "unknown"
+				}
+				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			}()
+			next.ServeHTTP(w, r)
+		});
+	}
+}
+
+func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get("X-Request-Id")
+			if requestID == "" {
+				requestID = nextRequestID()
+			}
+			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+			w.Header().Set("X-Request-Id", requestID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
 	}
 }
